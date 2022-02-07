e341573a55e451dac32e4463e5388069a52492d8
Christian Mehring <cmeh@mailbox.org>
2022-02-07 19:17:55 +0100

    Add test for handling multiple categories in parse_categories

--- tests/classifieds/conftest.py
+++ tests/classifieds/conftest.py
@@ -11,3 +11,8 @@ def fixture(file_name: str) -> str:
 @pytest.fixture
 def html_for_single_category() -> str:
     return fixture("single_category.html")
+
+
+@pytest.fixture
+def html_for_multiple_categories() -> str:
+    return fixture("multiple_categories.html")
--- tests/classifieds/test_categories.py
+++ tests/classifieds/test_categories.py
@@ -13,3 +13,31 @@ def test_parse_categories_with_single_category(html_for_single_category):
     ]
 
     assert parse_categories(html_for_single_category) == expected_categories
+
+
+def test_parse_categories_with_multiple_categories(html_for_multiple_categories):
+    expected_categories = [
+        Category(
+            category_type="verkauf-angebote",
+            slug="arbeitsplatzangebote-gemeinnuetzig",
+            title="Jobangebote gemeinnütziger Einrichtungen",
+            classified_count=218,
+            url="https://schwarzesbrett.bremen.de/verkauf-angebote/rubrik/arbeitsplatzangebote-gemeinnuetzig.html"
+        ),
+        Category(
+            category_type="kauf-gesuche",
+            slug="arbeitsplatzgesuche-kauf",
+            title="Arbeitsplatzgesuche",
+            classified_count=82,
+            url="https://schwarzesbrett.bremen.de/kauf-gesuche/rubrik/arbeitsplatzgesuche-kauf.html"
+        ),
+        Category(
+            category_type="diverses",
+            slug="gemeinschaftliches-wohnen",
+            title="Bau- und Mietgemeinschaftsprojekte",
+            classified_count=5,
+            url="https://schwarzesbrett.bremen.de/diverses/rubrik/gemeinschaftliches-wohnen.html"
+        ),
+    ]
+
+    assert parse_categories(html_for_multiple_categories) == expected_categories

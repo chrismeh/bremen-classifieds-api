4ab9db5df79e2c5365ddf45284d6108ea4aba052
Christian Mehring <cmeh@mailbox.org>
2022-02-07 19:45:05 +0100

    Implement logic for parsing a single classified from an HTML string

--- bremen_classifieds_api/classifieds/classifieds.py
+++ bremen_classifieds_api/classifieds/classifieds.py
@@ -1,6 +1,9 @@
+import re
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List
+
+from bs4 import BeautifulSoup
 
 
 @dataclass
@@ -17,4 +20,17 @@ class Classified:
 
 
 def parse_classifieds(html: str) -> List[Classified]:
-    pass
+    soup = BeautifulSoup(html, "html.parser")
+    classified = soup.select(".eintraege_list li a")[0]
+
+    return [Classified(
+        id=int(re.match(r".+?(\d+)\.html$", classified.attrs["href"]).group(1)),
+        category_type=classified.attrs["href"].split("/")[1],
+        category_slug=classified.attrs["href"].split("/")[2],
+        slug=re.sub(r"-(\d+)\.html", "", classified.attrs["href"].split("/").pop()),
+        title=re.sub(r" (?:\d{2}\.?){3}", "", ' '.join(classified.text.split())),
+        date=datetime.strptime(classified.select_one(".list_date").text.strip(), "%d.%m.%y").date(),
+        url="https://schwarzesbrett.bremen.de" + classified.attrs["href"],
+        has_picture=classified.select_one(".fa-camera") is not None,
+        is_commercial=classified.select_one(".fa-eur") is not None,
+    )]

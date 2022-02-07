274d4d13b86a8edcd90797c5e743ae992b9d6af8
Christian Mehring <cmeh@mailbox.org>
2022-02-07 20:37:31 +0100

    Improve error handling for get_categories

--- bremen_classifieds_api/classifieds/client.py
+++ bremen_classifieds_api/classifieds/client.py
@@ -5,6 +5,10 @@ import requests
 from bremen_classifieds_api.classifieds import Category, parse_categories
 
 
+class HttpError(Exception):
+    pass
+
+
 class Client:
     base_url = "https://schwarzesbrett.bremen.de/"
 
@@ -13,4 +17,7 @@ class Client:
 
     def get_categories(self) -> List[Category]:
         response = self.__session.get(self.base_url)
+        if response.status_code != 200:
+            raise HttpError(f"expected http status code 200 for {self.base_url}, got {response.status_code}")
+
         return parse_categories(response.text)
--- tests/classifieds/test_client.py
+++ tests/classifieds/test_client.py
@@ -1,7 +1,8 @@
 import httpretty
+import pytest
 import requests
 
-from bremen_classifieds_api.classifieds import Category, Client
+from bremen_classifieds_api.classifieds import Category, Client, HttpError
 from tests.classifieds.conftest import fixture
 
 
@@ -20,3 +21,19 @@ def test_client_get_categories():
     assert type(response) is list
     assert len(response) == 1
     assert type(response.pop()) is Category
+
+
+@httpretty.activate
+def test_client_get_categories_with_http_error():
+    httpretty.register_uri(
+        method=httpretty.GET,
+        uri=Client.base_url,
+        status=500
+    )
+
+    client = Client(requests.session())
+
+    with pytest.raises(HttpError) as err:
+        client.get_categories()
+
+    assert str(err.value) == f"expected http status code 200 for {Client.base_url}, got 500"

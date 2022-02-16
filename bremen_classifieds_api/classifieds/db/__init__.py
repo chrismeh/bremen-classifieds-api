470e8ba79caca4386c264f28820c3aa4ee8453f9
Christian Mehring <cmeh@mailbox.org>
2022-02-16 20:25:54 +0100

    Move object-database mapping to dedicated class

--- bremen_classifieds_api/classifieds/db/__init__.py
+++ bremen_classifieds_api/classifieds/db/__init__.py
@@ -20,7 +20,7 @@ class ClassifiedRepository:
 
         with self._db.cursor() as cursor:
             cursor.execute(query, (category.id,))
-            return [self._to_object(row) for row in cursor]
+            return [ClassifiedMapper.to_object(row) for row in cursor]
 
     def insert_many(self, category: Category, classifieds: Iterable[NewClassified]):
         query = """
@@ -31,10 +31,13 @@ class ClassifiedRepository:
         """
 
         with self._db.cursor(prepared=True) as cursor:
-            params = [self._to_database(category, classified) for classified in classifieds]
+            params = [ClassifiedMapper.to_database(category, classified) for classified in classifieds]
             cursor.executemany(query, params)
 
-    def _to_database(self, category: Category, classified: NewClassified) -> tuple:
+
+class ClassifiedMapper:
+    @staticmethod
+    def to_database(category: Category, classified: NewClassified) -> tuple:
         return (
             classified.id,
             category.id,
@@ -46,11 +49,10 @@ class ClassifiedRepository:
             classified.date
         )
 
-    def _to_object(self, row: tuple) -> Classified:
+    @staticmethod
+    def to_object(row: tuple) -> Classified:
         category_data, classified_data = row[:8], row[8:]
-
-        category_id, type, slug, title, classified_count, url, created_at, updated_at = category_data
-        category = Category(category_id, type, slug, title, classified_count, url, created_at, updated_at)
+        category = CategoryMapper.to_object(category_data)
 
         classified_id, _, slug, title, url, has_picture, is_commercial, publish_date, created_at, updated_at = classified_data
         return Classified(classified_id, category, slug, title, publish_date, url, has_picture, is_commercial,
@@ -66,7 +68,7 @@ class CategoryRepository:
 
         with self._db.cursor() as cursor:
             cursor.execute(query)
-            return [self._to_object(row) for row in cursor]
+            return [CategoryMapper.to_object(row) for row in cursor]
 
     def find_by_id(self, category_id: int) -> Optional[Category]:
         query = "SELECT * FROM category WHERE `id` = %s LIMIT 1;"
@@ -75,7 +77,7 @@ class CategoryRepository:
             if (row := cursor.fetchone()) is None:
                 return None
 
-            return self._to_object(row)
+            return CategoryMapper.to_object(row)
 
     def insert_many(self, categories: Iterable[NewCategory]):
         query = """
@@ -87,12 +89,16 @@ class CategoryRepository:
         """
 
         with self._db.cursor(prepared=True) as cursor:
-            params = [self._to_database(category) for category in categories]
+            params = [CategoryMapper.to_database(category) for category in categories]
             cursor.executemany(query, params)
 
-    def _to_database(self, category: NewCategory) -> tuple:
+
+class CategoryMapper:
+    @staticmethod
+    def to_database(category: NewCategory) -> tuple:
         return category.category_type, category.slug, category.title, category.classified_count, category.url
 
-    def _to_object(self, row: tuple) -> Category:
+    @staticmethod
+    def to_object(row: tuple) -> Category:
         category_id, category_type, slug, title, classified_count, url, created_at, updated_at = row
         return Category(category_id, category_type, slug, title, classified_count, url, created_at, updated_at)

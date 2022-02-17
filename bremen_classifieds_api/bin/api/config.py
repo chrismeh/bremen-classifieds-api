8ca494295e80f6b30f8101507fcbf7ed1a328281
Christian Mehring <cmeh@mailbox.org>
2022-02-17 17:41:35 +0100

    Remove obsolete caching config

--- bremen_classifieds_api/bin/api/config.py
+++ bremen_classifieds_api/bin/api/config.py
@@ -6,8 +6,6 @@ env.read_env()
 ENV = env.str("FLASK_ENV", "production")
 DEBUG = ENV == "development"
 JSON_SORT_KEYS = False
-CACHE_TYPE = env.str("CACHE_TYPE", "SimpleCache")
-CACHE_DEFAULT_TIMEOUT = env.int("CACHE_TIMEOUT", 0)
 
 MYSQL_HOST = env.str("MYSQL_HOST", "localhost")
 MYSQL_PORT = env.int("MYSQL_PORT", 3306)

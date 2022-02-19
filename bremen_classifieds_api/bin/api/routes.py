5f9f783c8b043b83baa5673c84ba213613e2b8a8
Christian Mehring <cmeh@mailbox.org>
2022-02-19 10:02:37 +0100

    Remove query parameters to trigger an update
    Also remove redundant default None in calls to get

--- bremen_classifieds_api/bin/api/routes.py
+++ bremen_classifieds_api/bin/api/routes.py
@@ -1,10 +1,8 @@
-import requests
 from flask import jsonify, Blueprint, abort, request
 
 from bremen_classifieds_api.bin.api.extensions import db
 from bremen_classifieds_api.bin.api.schemas import CategorySchema, ClassifiedsSchema
 from bremen_classifieds_api.classifieds.classifieds import Filter
-from bremen_classifieds_api.classifieds.client import Client
 from bremen_classifieds_api.classifieds.db import CategoryRepository, ClassifiedRepository
 
 bp = Blueprint("categories", __name__, url_prefix="/api/categories")
@@ -13,11 +11,6 @@ bp = Blueprint("categories", __name__, url_prefix="/api/categories")
 @bp.get("/")
 def get_categories():
     repo = CategoryRepository(db.connection)
-
-    if request.args.get("update"):
-        client = Client(requests.session())
-        categories = client.get_categories()
-        repo.insert_many(categories)
 
     categories = repo.find_all()
     return jsonify(CategorySchema(many=True).dump(categories))
@@ -32,24 +25,19 @@ def get_classifieds_by_category(category_id: int):
     if category is None:
         abort(404)
 
-    if request.args.get("update"):
-        client = Client(requests.session())
-        classifieds = client.get_classifieds(category)
-        classifieds_repo.insert_many(category, classifieds)
-
     classifieds_list = classifieds_repo.find_all(category, parse_classifieds_filter())
     return jsonify(ClassifiedsSchema(many=True).dump(classifieds_list))
 
 
 def parse_classifieds_filter() -> Filter:
     f = Filter()
-    if (search := request.args.get("search", None)) is not None:
+    if (search := request.args.get("search")) is not None:
         f.search = search
 
-    if (has_picture := request.args.get("has_picture", None)) is not None:
+    if (has_picture := request.args.get("has_picture")) is not None:
         f.has_picture = has_picture == "1"
 
-    if (is_commercial := request.args.get("is_commercial", None)) is not None:
+    if (is_commercial := request.args.get("is_commercial")) is not None:
         f.is_commercial = is_commercial == "1"
 
     return f
